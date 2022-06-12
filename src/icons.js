da4ef2f078a2b4524f66ccd97ff488d2b8f3b81c
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-06-12 16:59:48 +0530

    Inital Commit

---
+++ src/icons.js
@@ -0,0 +1,17 @@
+import { BiMicrophone, BiMicrophoneOff } from "react-icons/bi";
+import { BsCameraVideoFill, BsCameraVideoOffFill } from "react-icons/bs";
+import {
+  MdCallEnd,
+  MdOutlineStopScreenShare,
+  MdOutlineScreenShare,
+} from "react-icons/md";
+
+export {
+  BiMicrophone,
+  BiMicrophoneOff,
+  BsCameraVideoFill,
+  BsCameraVideoOffFill,
+  MdCallEnd,
+  MdOutlineStopScreenShare,
+  MdOutlineScreenShare,
+};
